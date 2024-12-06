827ca0d7deddf3e773f2a3d776cba57f50528604
BugHunter-X <askzenai@gmail.com>
2024-12-06 07:32:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  // Missing error handling for invalid userId
+  db.getUser(userId, (err, user) => {
+    if (err) {
+      // ... handle the error, probably send 500
+    } else {
+      res.json(user);
+    }
+  });
+});
